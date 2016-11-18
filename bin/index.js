8d858c2e309c877295097de1899a79b7703fa65e
Lee Cookson <lee.cookson@wsj.com>
2016-11-17 21:03:50 -0500

    add matcher examples

--- bin/index.js
+++ bin/index.js
@@ -1,19 +1,53 @@
 const _ = require('lodash');
 
+const degreeMatch = require('../util/match/degree');
+
 const party = require('../generators/party');
 const bill = require('../generators/bill');
+const voter = require('../generators/voter');
 
 const numParties = process.argv[2] || 10;
-const numBills = process.argv[2] || 10;
+const numBills = process.argv[3] || 10;
+const numVoters = process.argv[4] || 10;
+
+console.log('argv', process.argv);
+
+var parties = [],
+  bills = [],
+  voters = [];
 
 console.log('Parties:');
 for (var i = 0; i < numParties; i++) {
   let p = party.create();
   console.log(p);
+  p.voters = [];
+  parties.push(p);
 }
 
 console.log('Bills:');
 for (var j = 0; j < numBills; j++) {
-  let b = bill.create({elements: _.random(1,3)});
+  let b = bill.create({elements: _.random(1,2)});
   console.log(b);
+  bills.push(b);
 }
+
+console.log('Voters:');
+
+for (var k = 0; k < numVoters; k++) {
+  let v = voter.create();
+  console.log(v);
+  v.parties = [];
+  voters.push(v);
+}
+
+_.forEach(parties, (party, pIndex) => {
+  _.forEach(voters, (voter) => {
+    let match = degreeMatch.simpleMatch(voter, parties[pIndex], 4);
+    if (match) {
+      voter.parties.push(party);
+      party.voters.push(voter);
+    }
+  });
+
+  console.log('party', pIndex, 'has', party.voters.length, 'from', numVoters);
+});
